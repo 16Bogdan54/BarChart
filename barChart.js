adcc405a7a90e3ac7c371e911b5923f2711126cd
16Bogdan54 <Yunakov74@gmail.com>
2021-08-24 19:14:03 +0300

    finished bar chart

--- barChart.js
+++ barChart.js
@@ -1,4 +1,3 @@
-
 /*
 *
 *    file includes:
@@ -6,7 +5,6 @@
          2) Dropdown legend
 *
 */
-
 
 // data
 let numbers = [];
@@ -19,7 +17,7 @@ for(let i = 0; i < 12; i++) {
     numbers3.push(Math.floor(Math.random() * 50)) + 1;
 }
 
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('barChart');
 
 const DISPLAY = true;
 const BORDER = false;
@@ -33,38 +31,17 @@ let myChart = new Chart(ctx, {
         datasets: [{
             label: 'Population',
             data: numbers,
-            backgroundColor: [
-                'rgba(128, 0, 0, 0.5)',
-                'rgba(54, 145, 235, 0.5)',
-                'rgba(188, 145, 168, 0.5)',
-                'rgba(75, 192, 192, 0.5)',
-                'rgba(153, 102, 255, 0.5)',
-                'rgba(255, 159, 64, 0.5)'
-            ],
+            backgroundColor: 'rgba(128, 0, 0, 0.5)',
         },
         {
             label: 'GDP',
             data: numbers2,
-            backgroundColor: [
-                'rgba(242, 84, 45, 0.5)',
-                'rgba(115, 93, 120, 0.5)',
-                'rgba(112, 169, 0, 0.5)',
-                'rgba(240, 135, 0, 0.5)',
-                'rgba(161, 74, 118, 0.5)',
-                'rgba(123, 30, 122, 0.5)'
-            ],
+            backgroundColor: 'rgba(242, 84, 45, 0.5)',
         },
         {
             label: 'Something else',
             data: numbers3,
-            backgroundColor: [
-                'rgba(64, 71, 109, 0.5)',
-                'rgba(235, 101, 52, 0.5)',
-                'rgba(254, 109, 115, 0.5)',
-                'rgba(34, 124, 157, 0.5)',
-                'rgba(219, 50, 77, 0.5)',
-                'rgba(255, 127, 17, 0.5)'
-            ],
+            backgroundColor: 'rgba(64, 71, 109, 0.5)',
         }
     ] // data set closing bracket 
     },
@@ -87,6 +64,7 @@ let myChart = new Chart(ctx, {
                 }
             },
             x: {
+                stacked: true,
                 grid: {
                     display: DISPLAY,
                     drawBorder: BORDER,
